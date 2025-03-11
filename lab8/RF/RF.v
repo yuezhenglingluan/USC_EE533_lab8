2ad92b2f4c44e95be98584c36a952fe91dd8f9b5
Ling <github@ling.moe>
2025-03-10 18:03:11 -0700

    update RF

--- lab8/RF/RF.v
+++ lab8/RF/RF.v
@@ -10,8 +10,8 @@ module RF
     input [2:0] r0addr,
     input [2:0] r1addr,
 
-    output reg signed [63:0] r0data,
-    output reg signed [63:0] r1data
+    output signed [63:0] r0data,
+    output signed [63:0] r1data
 );
 
     reg signed [63:0] RF [7:0];
@@ -31,10 +31,11 @@ module RF
         end
     end
 
-    always @(*) begin
-        r0data = ((waddr == r0addr) && wena) ? RF[waddr] : RF[r0addr];
-        r1data = ((waddr == r1addr) && wena) ? RF[waddr] : RF[r1addr];
-    end
-
+    // always @(*) begin
+    //     r0data = ((waddr == r0addr) && wena) ? RF[waddr] : RF[r0addr];
+    //     r1data = ((waddr == r1addr) && wena) ? RF[waddr] : RF[r1addr];
+    // end
+        assign r0data = ((waddr == r0addr) && wena) ? RF[waddr] : RF[r0addr];
+        assign r1data = ((waddr == r1addr) && wena) ? RF[waddr] : RF[r1addr];
 
 endmodule
